63c091a3de04e26d7f45e73bc001b6493d034aa8
Danil Podobin <41650868+Danil-Podobin@users.noreply.github.com>
2024-12-01 18:26:45 +0300

    Add files via upload

---
+++ src/libs/i18n.ts
@@ -0,0 +1,23 @@
+import i18next from 'i18next';
+
+import * as en from 'src/libs/translations/en.json';
+import * as zh_CN from 'src/libs/translations/zh-CN.json';
+import * as ru_RU from 'src/libs/translations/ru-RU.json';
+
+import { moment } from 'obsidian';
+
+i18next.init({
+	lng: moment.locale(),
+	fallbackLng: {
+		'zh-TW': ['zh-CN', 'en'],
+                'ru-RU': ['ru-RU', 'en'],
+		default: ['ru'],
+	},
+	resources: {
+		en: en,
+		'zh-CN': zh_CN,
+                'ru-RU': ru_RU,
+	},
+});
+
+export const i18n = i18next;
